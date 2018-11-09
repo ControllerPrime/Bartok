c0913a1820929ddbef2356597aa9b668f72ed164
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2018-11-08 19:32:27 -0500

    It's only the beginning

---
+++ Assets/__Scripts/CardBartok.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CBState includes both states for the game and to ... states for movement
+public enum CBState
+{
+    toDrawpile,
+    drawpile,
+    toHand,
+    hand,
+    toTarget,
+    target,
+    discard,
+    to,
+    idle
+}
+
+public class CardBartok : Card
+{
+    static public float MOVE_DURATION = 0.5f;
+    static public string MOVE_EASING = Easing.InOut;
+    static public float CARD_HEIGHT = 3.5f;
+    static public float CARD_WIDTH = 2f;
+
+    [Header("set Dynamically: CardBartok")]
+    public CBState state = CBState.drawpile;
+
+    //fields to store info the card will use to move and rotate
+    public List<Vector3> bezierPts;
+    public List<Quaternion> bezierRots;
+    public float timeStart, timeDuration;
+
+    // when the card is done moving, it will call reportFinishTo.SendMessage()
+    public GameObject reportFinishTo = null;
+
+    //MoveTo tells the card to interpolate to a new pos and rotation
+    public void MoveTo(Vector3 ePos, Quaternion eRot)
+    {
+        // make a new interpolation lists for the card
+        // pos and rot will each only have 2 pts
+
+        // current
+        bezierPts = new List<Vector3>();
+        bezierPts.Add(transform.localPosition);
+        bezierPts.Add(ePos);
+
+        // new
+        bezierRots = new List<Quaternion>();
+        bezierRots.Add(transform.rotation);
+        bezierRots.Add(eRot);
+
+        if(timeStart == 0)
+        {
+            timeStart = Time.time;
+        }
+        // timeDuration always starts the same but can be overwritten
+        timeDuration = MOVE_DURATION;
+
+        state = CBState.to;
+    }
+
+    public void MoveTo(Vector3 ePos)
+    {
+        MoveTo(ePos, Quaternion.identity);
+    }
+    
+	// Update is called once per frame
+	void Update ()
+    {
+		switch(state)
+        {
+            case CBState.toHand:
+            case CBState.toTarget:
+            case CBState.toDrawpile:
+            case CBState.to:
+                float u = (Time.time - timeStart) / timeDuration;
+                float uC = Easing.Ease(u, MOVE_EASING);
+
+                if(u < 0)
+                {
+                    transform.localPosition = bezierPts[0];
+                    transform.rotation = bezierRots[0];
+                    return;
+                }
+                else if(u >= 1)
+                {
+                    uC = 1;
+
+                    // move from the to state to... the proper next state
+                    if (state == CBState.toHand) state = CBState.hand;
+                    if (state == CBState.toTarget) state = CBState.target;
+                    if (state == CBState.toDrawpile) state = CBState.drawpile;
+                    if (state == CBState.to) state = CBState.idle;
+
+                    // move to the final pos
+                    transform.localPosition = bezierPts[bezierPts.Count - 1];
+                    transform.rotation = bezierRots[bezierPts.Count - 1];
+
+                    // reset timeStart to 0 so it gets overwritten next time
+                    timeStart = 0;
+
+                    if(reportFinishTo != null)
+                    {
+                        reportFinishTo.SendMessage("CBCallback", this);
+                        reportFinishTo = null;
+                    }
+                    else
+                    {
+                        // if there is nothing to callback, keep it still
+                    }
+                }
+                else
+                {
+                    // normal interpolation behavior (0 <= u <= 1)
+                    Vector3 pos = Utils.Bezier(uC, bezierPts);
+                    transform.localPosition = pos;
+                    Quaternion rotQ = Utils.Bezier(uC, bezierRots);
+                    transform.rotation = rotQ;
+                }
+                break;
+        }
+	}
+}
